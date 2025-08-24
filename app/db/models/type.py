11de96795c8d372b12ff5bf750e3b9a717f2b340
Abylai Nurlanuly <itrider@Abylais-MacBook-Pro.local>
2025-08-24 12:33:25 +0500

    Added created and updated fields to all models

--- app/db/models/type.py
+++ app/db/models/type.py
@@ -1,5 +1,6 @@
 import uuid
 
+from datetime import datetime
 from sqlmodel import Field
 
 from app.schemas.type import TypeBase
@@ -7,3 +8,5 @@ from app.schemas.type import TypeBase
 
 class Type(TypeBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
